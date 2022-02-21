a58126ec87d56c12e0d9e52820d67f60e018785e
fratorgano <francesco.torgano@outlook.it>
2022-02-21 20:50:58 +0100

    Update survey

--- public/javascripts/survey.js
+++ public/javascripts/survey.js
@@ -3,7 +3,7 @@ const socket = io();
 
 $('#clearSurveyButton').on('click', clearSurvey);
 
-const difficulties = ['RandomPlay', 'GreedyMiniMax2A075', 'ABMMT3K04', 'ABMMT2K04'];
+const difficulties = ['RandomPlay', 'GreedyMiniMax2A075', 'ABMMT2K04', 'ABMMT2K04'];
 
 // https://surveyjs.io/create-survey
 /* const surveys = [
@@ -13,11 +13,56 @@ const difficulties = ['RandomPlay', 'GreedyMiniMax2A075', 'ABMMT3K04', 'ABMMT2K0
   JSON.parse('{"pages":[{"name":"beforePlaying","elements":[{"type":"text","name":"name","title":"What\'s your name?","isRequired":true,"readOnly":true,"placeHolder":"It doesn\'t need to be the real one"},{"type":"text","name":"surname","title":"What\'s your surname?","isRequired":true,"readOnly":true,"placeHolder":"It doesn\'t need to be the real one"},{"type":"rating","name":"ownStrength","title":"How strong do you think you are at UNO?","isRequired":true,"rateMin": 0, "rateMax": 10}],"title":"Before playing"},{"name":"afterGame1","elements":[{"type":"rating","name":"bot1Strength","title":"How strong was the opponent?","rateMin": 0, "rateMax": 10},{"type":"rating","name":"bot1Fun","title":"How much fun did you have?","rateMin":0,"rateMax":10}],"title":"First game","description":"Please answer with information regarding the first game"},{"name":"afterGame2","elements":[{"type":"rating","name":"bot2Strength","title":"How strong was the opponent?","rateMin": 0, "rateMax": 10},{"type":"rating","name":"bot2Fun","title":"How much fun did you have?","rateMin":0,"rateMax":10}],"title":"Second game","description":"Please answer with information regarding the second game"},{"name":"AfterGame3","elements":[{"type":"rating","name":"bot3Strength","title":"How strong was the opponent?","rateMin": 0, "rateMax": 10},{"type":"rating","name":"bot3Fun","title":"How much fun did you have?","rateMin":0,"rateMax":10}],"title":"Third game","description":"Please answer with information regarding the third game"},{"name":"afterPlaying","elements":[{"type":"ranking","name":"sortDifficulty","title":"Sort the games by how difficult they were","choices":[{"value":"game1","text":"Game 1"},{"value":"game2","text":"Game 2"},{"value":"game3","text":"Game 3"}]},{"type":"ranking","name":"sortFun","title":"Sort the games by how fun they were","choicesFromQuestion": "sortDifficulty"}],"title":"After playing"}],"completeText":"Done"}'),
 ]; */
 // survey generation
-const surveyBaseString = '{"pages":[],"completeText":"Next"}';
+const surveyBaseString = '{"pages":[],"completeText":"Prossima"}';
 const pages = [];
-const firstPageString = '{"name":"beforePlaying","elements":[{"type":"text","name":"name","title":"What\'s your name?","isRequired":true,"placeHolder":"It doesn\'t need to be the real one"},{"type":"text","name":"surname","title":"What\'s your surname?","isRequired":true,"placeHolder":"It doesn\'t need to be the real one"},{"type":"rating","name":"ownStrength","title":"How strong do you think you are at UNO?","isRequired":true,"rateMin": 0, "rateMax": 10}],"title":"Before playing"}';
+const firstPage = {
+  name: 'beforePlaying',
+  elements: [
+    {
+      type: 'text',
+      name: 'name',
+      title: 'Inserisci un username',
+      isRequired: true,
+      placeHolder: 'Insert username',
+    },
+    {
+      type: 'nouislider',
+      name: 'ownStrength',
+      title: 'Quanto forte credi di essere a uno?',
+      step: 1,
+      rangeMin: 0,
+      rangeMax: 100,
+      pipsValues: [0, 50, 100],
+      pipsText: [{ value:0, text:'Scarso' }, { value:50, text:'Nella media' }, { value:100, text:'Forte' }],
+      pipsDensity: 100,
+      tooltips: false,
+    },
+    {
+      type: 'nouislider',
+      name: 'funAgainstAI',
+      title: 'Quanto credi possa essere divertente giocare contro una AI?',
+      step: 1,
+      rangeMin: 0,
+      rangeMax: 100,
+      pipsValues: [0, 50, 100],
+      pipsText: [{ value:0, text:'Noioso' }, { value:50, text:'Normale' }, { value:100, text:'Divertente' }],
+      pipsDensity: 100,
+      tooltips: false,
+    },
+    {
+      type: 'rating',
+      name: 'predictionWonGames',
+      title: 'Quante partite credi di poter vincere?',
+      step: 1,
+      rateMin: 0,
+      rateMax: difficulties.length,
+    },
+  ],
+  title: 'Prepartita',
+};
+// const firstPageString = '{"name":"beforePlaying","elements":[{"type":"text","name":"name","title":"What\'s your username?","isRequired":true,"placeHolder":"It doesn\'t need to be the real one"},{"type":"rating","name":"ownStrength","title":"How strong do you think you are at UNO?","isRequired":true,"rateMin": 0, "rateMax": 10}],"title":"Before playing"}';
 // const lastPageString = '{"name":"afterPlaying","elements":[{"type":"ranking","name":"sortDifficulty","title":"Sort the games by how difficult they were","choices":[{"value":"game1","text":"Game 1"},{"value":"game2","text":"Game 2"},{"value":"game3","text":"Game 3"}]},{"type":"ranking","name":"sortFun","title":"Sort the games by how fun they were","choicesFromQuestion": "sortDifficulty"}],"title":"After playing"}';
-pages.push(JSON.parse(firstPageString));
+pages.push(firstPage);
 // generate pages for each game
 for (const key in difficulties) {
   const number = parseInt(key, 10);
@@ -25,23 +70,64 @@ for (const key in difficulties) {
     name: `afterGame${number + 1}`,
     elements: [
       {
-        type: 'rating',
+        type: 'nouislider',
         name: 'bot' + (number + 1) + 'Strength',
-        title: 'How strong was the opponent?',
-        rateMin: 0,
-        rateMax: 10,
+        title: 'Quanto era forte il tuo avversario?',
+        step: 1,
+        rangeMin: 0,
+        rangeMax: 100,
+        pipsValues: [0, 50, 100],
+        pipsText: [{ value:0, text:'Scarso' }, { value:50, text:'Nella media' }, { value:100, text:'Forte' }],
+        pipsDensity: 100,
+        tooltips: false,
       },
       {
-        type: 'rating',
+        type: 'boolean',
+        name: 'bot' + (number + 1) + 'IsAI',
+        title: 'Saresti in grado di capire se hai giocato contro una IA o contro un giocatore umano?',
+        labelTrue: 'Sì',
+        // defaultValue: 'true',
+      },
+      {
+        type: 'nouislider',
         name: 'bot' + (number + 1) + 'Fun',
-        title: 'How much fun did you have?',
-        rateMin: 0,
-        rateMax: 10,
+        title: 'Quanto ti sei divertito in questa partita?',
+        step: 1,
+        rangeMin: 0,
+        rangeMax: 100,
+        pipsValues: [0, 50, 100],
+        pipsText: [{ value:0, text:'Poco' }, { value:50, text:'Nella media' }, { value:100, text:'Tanto' }],
+        pipsDensity: 100,
+        tooltips: false,
+      },
+      {
+        type: 'radiogroup',
+        name: 'length' + (number + 1),
+        title: 'La durata della partita è stata:',
+        choices: [
+          'Corta',
+          'Giusta',
+          'Lunga',
+        ],
       },
     ],
-    title: `Game ${number + 1}`,
-    description: 'Please answer with information regarding the game ' + (number + 1),
+    title: `Partita ${number + 1}`,
+    description: 'Rispondi con informazioni riguardanti la partita ' + (number + 1),
   };
+  // add give up question if the game was gave up
+  const games = JSON.parse(window.localStorage.getItem('games'));
+  if (games && games[number] && games[number].winner === null) {
+    page.elements.push({
+      type: 'radiogroup',
+      name: 'bot' + (number + 1) + 'GiveUp',
+      title: 'Perchè ti sei arreso?',
+      choices: [
+        'Troppo difficile',
+        'Troppo lunga',
+        'Troppo noiosa',
+      ],
+    });
+  }
   pages.push(page);
 }
 // generate last page
@@ -50,7 +136,7 @@ for (const key in difficulties) {
   const number = parseInt(key, 10);
   const choice = {
     value: `game${number + 1}`,
-    text: `Game ${number + 1}`,
+    text: `Partita ${number + 1}`,
   };
   choices.push(choice);
 }
@@ -60,17 +146,39 @@ const lastPage = {
     {
       type: 'ranking',
       name: 'sortDifficulty',
-      title: 'Sort the games by how difficult they were',
+      title: 'Ordina le partite per difficoltà (più difficile prima)',
       choices: choices,
     },
     {
       type: 'ranking',
       name: 'sortFun',
-      title: 'Sort the games by how fun they were',
+      title: 'Ordina le partite per divertimento (più divertente prima)',
       choicesFromQuestion: 'sortDifficulty',
     },
+    {
+      type: 'nouislider',
+      name: 'generalFun',
+      title: 'Quanto ti sei divertito in generale?',
+      step: 1,
+      rangeMin: 0,
+      rangeMax: 100,
+      pipsValues: [0, 50, 100],
+      pipsText: [{ value:0, text:'Poco' }, { value:50, text:'Nella media' }, { value:100, text:'Tanto' }],
+      pipsDensity: 100,
+      tooltips: false,
+    },
+    {
+      type: 'radiogroup',
+      name: 'lengthTotal',
+      title: 'La durata del sondaggio è:',
+      choices: [
+        'Corta',
+        'Giusta',
+        'Lunga',
+      ],
+    },
   ],
-  title: 'After playing',
+  title: 'Postpartita',
 };
 pages.push(lastPage);
 
@@ -145,6 +253,7 @@ function initializeSurvey() {
   }
   function savingPartialData(s) {
     // saving on local storage
+    console.log(s.getPlainData());
     const data = s.data;
     data.pageNo = s.currentPageNo;
     window.localStorage.setItem('survey', JSON.stringify(data));
@@ -159,10 +268,10 @@ function pickSurvey() {
   const survey = JSON.parse(surveyBaseString);
   // update complete text to match game
   if (parseInt(gameNumber, 10) === difficulties.length) {
-    survey.completeText = 'Done';
+    survey.completeText = 'Finito';
   }
   else {
-    survey.completeText = 'Play game ' + (parseInt(gameNumber, 10) + 1);
+    survey.completeText = 'Gioca partita ' + (parseInt(gameNumber, 10) + 1);
   }
 
   // add pages until gameNumber
@@ -180,10 +289,10 @@ function pickSurvey() {
     const parsedId = parseInt(id);
     const game = games[parsedId];
     if (game.winner) {
-      survey.pages[parsedId + 1].description = `The games was won by ${game.winner} in ${game.turns} turns.`;
+      survey.pages[parsedId + 1].description = `La partita è stata vinta da ${game.winner} in ${game.turns} turni.`;
     }
     else {
-      survey.pages[parsedId + 1].description = `You gave up after ${game.turns} turns.`;
+      survey.pages[parsedId + 1].description = `Ti sei arreso dopo ${game.turns} turni.`;
     }
   }
   return survey;
@@ -210,7 +319,7 @@ function createRoom(name, surname) {
   console.log('creating room...');
   // saving name & surname
   document.cookie = `name=${name}; SameSite=Strict`;
-  document.cookie = `surname=${surname}; SameSite=Strict`;
+  document.cookie = 'surname=; SameSite=Strict';
   document.cookie = 'gameType="singleplayer"; SameSite=Strict';
   // creating room for game
   // TODO: change difficulties
@@ -226,6 +335,15 @@ socket.on('room_created', (uuid) => {
 });
 
 window.onload = () => {
+  const prevData = window.localStorage.getItem('survey') || null;
+  if (prevData) {
+    console.log('old survey data found, hiding disclaimer and rules');
+    $('#survey-information').hide();
+  }
+  else {
+    console.log('no old survey data found, showing disclaimer and rules');
+    $('#survey-information').show();
+  }
   /* console.log('loading data from cookies');
   if (getCookie('roomName') != '') {
     roomNameInput.value = getCookie('roomName');
