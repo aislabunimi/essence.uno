9555b04f2d4b171362beea0866f6e120e7aa01d6
fratorgano <francesco.torgano@outlook.it>
2022-03-09 18:14:34 +0100

    Add favicon

--- app.js
+++ app.js
@@ -43,6 +43,9 @@ app.use('/', indexRouter);
 app.use('/game', gameRouter);
 app.use('/survey', surveyRouter);
 
+// add favicon
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'favicon.ico')));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
